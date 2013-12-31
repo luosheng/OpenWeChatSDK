9b9b4cab1395cfa969efda29965f7212262a9874
Sheng Luo <luosheng1986@gmail.com>
2013-12-31 16:49:03 +0800

    Put a segmented control in the title view.

--- Example/Example/LSViewController.m
+++ Example/Example/LSViewController.m
@@ -10,20 +10,31 @@
 
 @interface LSViewController ()
 
+@property (nonatomic, readonly) UISegmentedControl *sceneSegmentedControl;
+
 @end
 
 @implementation LSViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+- (id)initWithCoder:(NSCoder *)aDecoder {
+	self = [super initWithCoder:aDecoder];
+	
+	if (self) {
+	}
+	return self;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	
+	_sceneSegmentedControl = [[UISegmentedControl alloc] initWithItems:@[@"会话", @"朋友圈", @"收藏"]];
+	_sceneSegmentedControl.selectedSegmentIndex = 0;
+	self.navigationItem.titleView = _sceneSegmentedControl;
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
 }
 
 @end
