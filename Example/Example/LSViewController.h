e8ac2196f407c22d595fbabfb4bdcd9ceca65af7
Sheng Luo <luosheng1986@gmail.com>
2013-12-31 17:10:17 +0800

    Add a snippet  to send text content to WeChat.

--- Example/Example/LSViewController.h
+++ Example/Example/LSViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "WXApi.h"
 
 @interface LSViewController : UITableViewController
 
--- Example/Example/LSViewController.m
+++ Example/Example/LSViewController.m
@@ -66,4 +66,14 @@ static NSString *const LSCellIdentifier = @"Cell";
 	return cell;
 }
 
+#pragma mark - WeChat API actions
+
+- (void)sendTextContent {
+	SendMessageToWXReq* req = [[SendMessageToWXReq alloc] init];
+	req.text = @"人文的东西并不是体现在你看得到的方面，它更多的体现在你看不到的那些方面，它会影响每一个功能，这才是最本质的。但是，对这点可能很多人没有思考过，以为人文的东西就是我们搞一个很小清新的图片什么的。”综合来看，人文的东西其实是贯穿整个产品的脉络，或者说是它的灵魂所在。";
+	req.bText = YES;
+	
+	[WXApi sendReq:req];
+}
+
 @end
