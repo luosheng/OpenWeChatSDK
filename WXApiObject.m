30ee894b93a0fc2867ee8f4f802efdfd2d9d7248
Sheng Luo <luosheng1986@gmail.com>
2013-12-31 17:08:38 +0800

    Insert empty implementations.

---
+++ WXApiObject.m
@@ -0,0 +1,52 @@
+#import "WXApiObject.h"
+
+@implementation BaseReq
+@end
+
+@implementation BaseResp
+@end
+
+@implementation SendMessageToWXReq
+@end
+
+@implementation SendMessageToWXResp
+@end
+
+@implementation GetMessageFromWXReq
+@end
+
+@implementation GetMessageFromWXResp
+@end
+
+@implementation ShowMessageFromWXReq
+@end
+
+@implementation ShowMessageFromWXResp
+@end
+
+@implementation LaunchFromWXReq
+@end
+
+@implementation WXMediaMessage
+@end
+
+@implementation WXImageObject
+@end
+
+@implementation WXMusicObject
+@end
+
+@implementation WXVideoObject
+@end
+
+@implementation WXWebpageObject
+@end
+
+@implementation WXAppExtendObject
+@end
+
+@implementation WXEmoticonObject
+@end
+
+@implementation WXFileObject
+@end
